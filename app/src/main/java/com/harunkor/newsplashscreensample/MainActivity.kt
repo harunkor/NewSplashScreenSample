54699dec9ca6108c8fd533e07e0ea5acce3845df
harunkor <harunkor@gmail.com>
2022-05-12 22:22:30 +0300

    Initial commit

---
+++ app/src/main/java/com/harunkor/newsplashscreensample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.harunkor.newsplashscreensample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
